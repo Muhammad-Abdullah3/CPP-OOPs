f22725b82e75d3f37a2d5e840d5fa2a692322c65
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2023-11-12 16:53:20 +0500

    new file:   Chapter2.CPPBasics/Comments.cpp

---
+++ Chapter2.CPPBasics/Comments.cpp
@@ -0,0 +1,11 @@
+//Comments.cpp
+//This is the same as First.cpp with comments
+#include<iostream>//Preprocessor Directive or library
+using namespace std; //'using' directive
+int main() //Main Function start
+{//Start function body
+    cout<<"Every age has it's own language.";
+    /*this was the statement with a 
+    multi line comment*/
+    return 0; //return statement
+}//End function body
