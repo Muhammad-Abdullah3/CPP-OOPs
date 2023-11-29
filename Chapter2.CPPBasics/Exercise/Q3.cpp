d9ff5faa8ee4e18143f00da597363983b645ba7b
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2023-11-29 20:26:26 +0500

    new file:   Chapter2.CPPBasics/Exercise/Q3.cpp new file:   Chapter2.CPPBasics/Exercise/Q3.exe new file:   Chapter2.CPPBasics/Exercise/Q4.cpp

---
+++ Chapter2.CPPBasics/Exercise/Q3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int x = 10;
+    cout<<x<<endl;
+    x +=10;
+    cout<<x<<endl;
+    --x;
+    cout<<x<<endl;
+    return 0;
+}
