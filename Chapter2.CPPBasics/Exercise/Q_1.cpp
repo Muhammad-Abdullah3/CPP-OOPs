e79863c6ba8d8ba9205ffba1342a531e6384450d
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2023-11-29 20:10:30 +0500

    new file:   Chapter2.CPPBasics/Exercise/Q_1.cpp new file:   Chapter2.CPPBasics/charvars.cpp

---
+++ Chapter2.CPPBasics/Exercise/Q_1.cpp
@@ -0,0 +1,14 @@
+/*Asumming there are 7.481 gallons in a cubic foot. 
+Write a program that asks the user to enter a number of gallons, and then displays equivalent in
+Cubic feet.*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Total gallons in a cubic feet 7.841
+    float cub_foot = 7.481;
+    //Declaring a variable to get number of gallons
+    float gal;
+    cout<<"Enter the total number of gallons: \a"; 
+    return 0;
+}
