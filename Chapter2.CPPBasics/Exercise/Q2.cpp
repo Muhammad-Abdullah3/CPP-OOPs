8ee2faafcb819080cd413e729cf841d74a2b0c5c
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2023-11-29 20:19:40 +0500

    new file:   Chapter2.CPPBasics/Exercise/Q2.cpp new file:   Chapter2.CPPBasics/Exercise/Q2.exe

---
+++ Chapter2.CPPBasics/Exercise/Q2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    cout<<setw(10)<<"1990"<<setw(10)<<"135"<<endl;
+    cout<<setw(10)<<"1991"<<setw(10)<<"7290"<<endl;
+    cout<<setw(10)<<"1992"<<setw(10)<<"11300"<<endl;
+    cout<<setw(10)<<"1993"<<setw(10)<<"16200"<<endl;
+    return 0;
+}
