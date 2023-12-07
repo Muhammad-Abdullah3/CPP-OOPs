9f35491b5c98de2ee3a1bcfc3bbe952f3a475629
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2023-12-07 22:19:23 +0500

    new file:   Practice_problems/Arrays/append_prob.cpp new file:   Practice_problems/Arrays/append_prob.exe

---
+++ Practice_problems/Arrays/append_prob.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int size1 = 4,size2=4;
+    int first[size1]={23,45,23,12};
+    int sec[size2] = {21,63,17,18};
+    int merge_arr[size1+size2];
+    int count = 0;
+    for(int z=0;z<(size1+size2);z++)
+    {
+        if(count<(size1-1))
+        {
+        merge_arr[z]=first[count];
+        count++;
+        }
+        else
+        {
+            count = 0;
+            merge_arr[z]=sec[count];
+            count++;
+        }
+    }
+    //loop to print out the merged array
+    for 
+    getch();
+    return 0;
+}
