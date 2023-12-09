3d4ba0c27210f34e7f969df749605ac79bf24e37
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2023-12-09 18:25:53 +0500

    modified:   Practice_problems/Arrays/Sorting/ascending_order.cpp 	new file:   Practice_problems/Arrays/Sorting/ascending_order.exe new file:   Practice_problems/Arrays/Sorting/insertion_sort.cpp 	new file:   Practice_problems/Arrays/Sorting/insertion_sort.exe 	new file:   Practice_problems/Arrays/Sorting/selection_sort.cpp 	new file:   Practice_problems/Arrays/Sorting/selection_sort.exe

--- Practice_problems/Arrays/Sorting/selection_sort.cpp
+++ Practice_problems/Arrays/Sorting/selection_sort.cpp
@@ -4,28 +4,28 @@ int main()
 {
     int size =5;//Size of the array
     int arr[size] = {0};
-    int min = 0; //swapping variable
+    int max = 0; //swapping variable
     //using loop to input the values in arrays
     cout<<"Enter elements of the array: ";
     for(int x=0;x<size;x++)
     {
         cin>>arr[x];
     }
-    min = arr[0];
+    max = arr[0];
     //Sorting
     for(int y=0;y<size;y++)
     {
         for(int z=0;z<size;z++)
         {
-            if(arr[z]>arr[z+1])
+            if(arr[z]<arr[z+1])
             {
-                min = arr[z+1];
+                max = arr[z+1];
                 arr[z+1] = arr[z];
-                arr[z] = min;
+                arr[z] = max;
             }
         }
     }
-    cout<<"Array after sorting in Ascending order: ";
+    cout<<"Array after sorting in Descending order: ";
     for(int i=0;i<size;i++)
     {
         cout<<arr[i]<<endl;
