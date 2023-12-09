51e8d1712b0d73ce54baba4b4168713964980dd4
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2023-12-09 08:07:46 +0500

    modified:   Practice_problems/Arrays/Sorting/ascending_order.cpp

--- Practice_problems/Arrays/Sorting/ascending_order.cpp
+++ Practice_problems/Arrays/Sorting/ascending_order.cpp
@@ -4,10 +4,19 @@ int main()
 {
     int size =5;//Size of the array
     int arr[size] = {0};
+    int min = 0; //swapping variable
     //using loop to input the values in arrays
-    for(int x=0;x<=3;x++)
+    cout<<"Enter elements of the array: ";
+    for(int x=0;x<size;x++)
     {
-        
+        cin>>arr[x];
     }
-    for(int i =0;i<=3;i++)
+    //Sorting
+    for(int y=0;y<size;y++)
+    {
+        for(int z=y;z<size;z++)
+        {
+            
+        }
+    }
 }
