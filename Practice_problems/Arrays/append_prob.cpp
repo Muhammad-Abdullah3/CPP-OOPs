094b06f96dabde2443dc5cf83f2ad49667b7e155
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2023-12-13 18:06:12 +0500

    modified:   Practice_problems/Arrays/append_prob.cpp modified:   Practice_problems/Arrays/append_prob.exe 	new file:   Practice_problems/Arrays/arr_2d.cpp 	new file:   Practice_problems/Arrays/arr_2d.exe 	new file:   Practice_problems/Arrays/conv_uppercase.cpp 	new file:   Practice_problems/Arrays/conv_uppercase.exe 	new file:   Practice_problems/Arrays/del_ele.cpp 	new file:   Practice_problems/Arrays/del_ele.exe 	new file:   Practice_problems/Arrays/gmon.out 	new file:   Practice_problems/Arrays/searching_element.cpp 	new file:   Practice_problems/Arrays/searching_element.exe 	new file:   Practice_problems/Arrays/sum_arrays.cpp 	new file:   Practice_problems/Arrays/sum_arrays.exe

--- Practice_problems/Arrays/append_prob.cpp
+++ Practice_problems/Arrays/append_prob.cpp
@@ -1,14 +1,25 @@
-
 #include<iostream>
 #include<conio.h>
 using namespace std;
 int main()
 {
-    int size1 = 4,size2=4;
-    int first[size1]={23,45,23,12};
-    int sec[size2] = {21,63,17,18};
+    int size1 = 4,size2=5;
+    int first[size1]={0};
+    int sec[size2] = {0};
     int merge_arr[size1+size2];
     int count = 0;
+    //Loop to get input of both arrays
+    cout<<"Enter the values in first array: ";
+    for(int i=0;i<size1;i++)
+    {
+    	cin>>first[i];
+	}
+	cout<<"Enter the values in second array: ";
+    for(int i=0;i<size2;i++)
+    {
+    	cin>>sec[i];
+	}
+    //Loop to merge both arrays
     for(int z=0;z<(size1+size2);z++)
     {
         if(count<(size1-1))
@@ -24,7 +35,7 @@ int main()
         }
     }
     //loop to print out the merged array
-    cout<<"Mergeed array: \n";
+    cout<<"Merged array: \n";
     for (int i=0;i<(size1+size2);i++)
     {
         cout<<merge_arr[i]<<endl;
