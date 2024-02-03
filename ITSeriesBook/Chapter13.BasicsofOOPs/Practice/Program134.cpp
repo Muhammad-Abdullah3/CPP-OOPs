8ec2046c00b7b9f05c113a8875682c858e7a5c68
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-02-03 07:55:32 +0500

    new file:   ITSeriesBook/Chapter13.BasicsofOOPs/Practice/Program133.cpp 	new file:   ITSeriesBook/Chapter13.BasicsofOOPs/Practice/Program133.exe 	new file:   ITSeriesBook/Chapter13.BasicsofOOPs/Practice/Program134.cpp

---
+++ ITSeriesBook/Chapter13.BasicsofOOPs/Practice/Program134.cpp
@@ -0,0 +1,29 @@
+/*Write a class Book with three data members BookID, Pages and Price. It also contains the 
+following member function.
+1- The get funciton is used to input values.
+2- The show function is used to display values.
+3- The set function is used to set the values of data members using parameter
+4- The get price function is used to return the value of price.
+
+The program should createtwo objects of the class and input values for these objects. 
+The program displays the details of the most costly book.
+*/
+#include<iostream>
+using namespace std;
+class Book
+{
+    private:
+    int bookId, pages
+    float price;
+    public:
+    void get()
+    {
+        cout<<"Enter the Book Id: ";cin>>bookId;
+        cout<<"Enter the Book Pages: ";cin>>pages;
+        cout<<"Enter the Book Price: ";cin>>price;
+    }
+    void show()
+    {
+        cout<<
+    }
+};
