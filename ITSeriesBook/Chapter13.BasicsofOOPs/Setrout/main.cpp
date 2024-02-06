eb02a8e0997a41ff7483de231b3bef45a98119cd
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-02-06 16:20:31 +0500

    new file:   ITSeriesBook/Chapter13.BasicsofOOPs/Setrout/main.cpp

---
+++ ITSeriesBook/Chapter13.BasicsofOOPs/Setrout/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<datetimeapi.h>
+#include<fstream>
+class User
+{
+    private:
+    string name;
+    string Date;
+    public:
+    void input()
+    {
+        cout<<"Enter your name:";
+        cin>>
+    }
+};
+int main()
+{
+    return 0;
+}
