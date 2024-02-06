f535de2b20ed3ef87c25ba4bce7f5f3cabd32c10
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-02-06 18:56:38 +0500

    modified:   ITSeriesBook/Chapter13.BasicsofOOPs/Setrout/main.cpp

--- ITSeriesBook/Chapter13.BasicsofOOPs/Setrout/main.cpp
+++ ITSeriesBook/Chapter13.BasicsofOOPs/Setrout/main.cpp
@@ -7,6 +7,8 @@ class User
     private:
     string name;
     string date;
+    string startTime;
+    string endTime;
     public:
     void input()
     {
@@ -14,6 +16,14 @@ class User
         getline(cin,name);
         cout<<"Enter Date:";
         getline(cin,date);
+    }
+    void sleepTime()
+    {
+        char choice;
+        cout<<"\nChose Sleep Start Time:";
+        cout<<"\na- 09:00 PM\tb- 09:15 PM\tc- 09:30 PM\tb- 09:45 PM\td- 10:00 PM";
+        cout<<"\ne- 10:15 PM\tf- 10:30 PM\tg- 10:45 PM\th- 11:00 PM\ti- 11:15 PM";
+        cout<<"\nj- 11:30 PM\tk- 11:45 PM\tl- 12:00 AM";
     }
 };
 int main()
