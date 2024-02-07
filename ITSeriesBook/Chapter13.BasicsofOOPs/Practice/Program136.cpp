9ea15659f4aaa24763ac8afdd77694255467d879
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-02-07 17:58:08 +0500

    modified:   ITSeriesBook/Chapter13.BasicsofOOPs/Practice/Program136.cpp

--- ITSeriesBook/Chapter13.BasicsofOOPs/Practice/Program136.cpp
+++ ITSeriesBook/Chapter13.BasicsofOOPs/Practice/Program136.cpp
@@ -1,2 +1,7 @@
-/*
+/*Write a class Array that contains an array of integers to store five values. It also contains
+the following member functions defined outside the class.
+1- The fill() function is used to fill the array with the values of array
+2- the display() function is used ot dispaly the values of array
+3- The max() function shows the maximum value in the array
+4- The min() function shows the minimum value in the array
 */
