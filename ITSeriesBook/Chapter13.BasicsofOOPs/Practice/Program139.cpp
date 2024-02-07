3d26b523b9cc4ebce1bea1920c2aa96d48a1c1e6
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-02-07 22:46:06 +0500

    new file:   ITSeriesBook/Chapter13.BasicsofOOPs/Practice/Program139.cpp

---
+++ ITSeriesBook/Chapter13.BasicsofOOPs/Practice/Program139.cpp
@@ -0,0 +1,10 @@
+/*Write a class Student that has marks and grade as data members. A constructor with 
+two parameters initializes data members with the given values and member function show()
+displays the values of data members. Create two objects and display the values.
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+}
