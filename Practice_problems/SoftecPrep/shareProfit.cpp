1af194fbf9b29a2cd18a8a07ee6097341d1e6964
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-02-13 10:43:57 +0500

    new file:   Practice_problems/SoftecPrep/shareProfit.cpp

---
+++ Practice_problems/SoftecPrep/shareProfit.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+long getMaxProfit(int[],int);
+int main()
+{
+    return 0;
+}
+long getMaxProfit(int arrPnl[],int k)
+{
+    long sumSubArra = 0;
+    
+}
